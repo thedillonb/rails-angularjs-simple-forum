197b768a7f830b7dd9734c059d6b7a9c39cb1af2
Dillon Buchanan <thedillonb@gmail.com>
2013-03-12 11:09:46 -0700

    Added Angular, Bootstrap, root route, and the application launch action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # We'll just use this as a launch point for our App
+  def index
+    # Render just the layout since this application is Angular driven
+    # our layout/application has all the angular logic and our controllers
+    # have no views for themselves. We just need a place to launch from
+    # and this happens to be it. So we have no view (thus :nothing => true)
+    # but we still want the layout (since it has the App bootstrap code)
+    render :layout => 'application', :nothing => true
+  end
 end
