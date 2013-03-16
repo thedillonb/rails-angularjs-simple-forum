f2f78ca0703e3f497d272e08a7317158c0b1f960
Dillon Buchanan <thedillonb@gmail.com>
2013-03-16 11:09:38 -0400

    Commenting

--- app/assets/angular/controllers/comment-controller.js
+++ app/assets/angular/controllers/comment-controller.js
@@ -1,16 +1,28 @@
 'use strict';
 
 var app = angular.module('app');
-
 app.controller('CommentsController', function($scope, Comment, $routeParams) {
+    //Grab all the comments from the server
     $scope.comments = Comment.query({forumId: $routeParams.id});
 
+    //Define a 'save' method which will be called from the view.
     $scope.save = function() {
-        var obj = new Comment({name: $scope.name, body: $scope.body, forumId: $routeParams.id})
+        //Create the comment object to be sent to the server
+        var obj = new Comment({name: $scope.name, body: $scope.body, forumId: $routeParams.id});
+
+        //Attempt a save to the back-end
         obj.$save(function(response) {
+
+            //If we're successful then add the response (the object as the server sees it)
+            // to our collection of comments
             $scope.comments.unshift(response);
+
+            //Empty the name & body
             $scope.name = $scope.body = ""
+
         }, function(response) {
+
+            //If there's a failure set the 'errors' scope variable so it'll be reflected in the view.
             $scope.errors = response.data.errors;
         });
     }
--- app/assets/angular/controllers/forum-controller.js
+++ app/assets/angular/controllers/forum-controller.js
@@ -3,26 +3,43 @@
 var app = angular.module('app');
 
 app.controller('ForumIndexController', function($scope, Forum) {
+    //Grab all forums from the server
     $scope.items = Forum.query();
 
+    //Destroy method for deleting a forum
     $scope.destroy = function(index) {
+
+        //Tell the server to remove the object
         Forum.remove({id: $scope.items[index].id}, function() {
+            //If successful, remove it from our collection
             $scope.items.splice(index, 1);
         });
     }
 });
 
 app.controller('ForumCreateController', function($scope, $location, Forum) {
+    //The save method which is called when the user wants to submit their data
     $scope.save = function() {
+
+        //Create the forum object to send to the back-end
         var forum = new Forum($scope.forum);
+
+        //Save the forum object
         forum.$save(function() {
+
+            //Redirect us back to the main page
             $location.path('/');
+
         }, function(response) {
+
+            //Post response objects to the view
             $scope.errors = response.data.errors;
         });
     }
 });
 
+//A controller to show the forum and all it's glory
 app.controller('ForumShowController', function($scope, Forum, Comment, $routeParams) {
+    //Grab the forum from the server
     $scope.forum = Forum.get({id: $routeParams.id})
 });
